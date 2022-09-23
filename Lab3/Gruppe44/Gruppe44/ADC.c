686c2a34f7f1ebb84eab20b30199995ebd8a1feb
markuslangklopp <97596800+markuslangklopp@users.noreply.github.com>
2022-09-23 16:53:09 +0200

    Lab 3 - not approved

---
+++ Lab3/Gruppe44/Gruppe44/ADC.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <avr/io.h>
+#include "util/delay.h"
+
+
+void adc_init (void){
+	
+	DDRD |= (1 << PD5); // Set port PD5 to 1
+	TCCR1A |= (1 << COM1A0)|(1 << COM1B0); // Timer/Counter1 Control Register A (TCCR1A): Setting bit 6 and 4 to 1 - "Toggle OCnA/OCnB on Compare Match."
+	TCCR1B |= (1 << WGM12)|(1 << CS10); // Timer/Counter1 Control Register B (TCCR1B): Setting bit 3 to 1 - "CTC-mode", and bit 0 to 1 "clkI/O/1 (No prescaling)"
+	OCR1A = 0x1; // Setting Compare Match bit - which bit the counter triggers on
+}
+
+void adc_read(void){
+	// SEND TO ADC
+	uint16_t channel = 0x01;
+	
+	volatile char * ext_mem = ( char *) 0x1400; // Set pointer at starting memory of ADC
+	ext_mem [ channel ] = 0xff; // Send random value to initiate
+	
+	// WAIT
+	_delay_us(60); // Is this one necessary?
+	
+	// READ FROM ADC
+	for (int i=0;i<4;i++) // Need to read 4 values - analog input 0 to 3
+	{
+		uint8_t number = ext_mem [ channel ]; // Read the ADC value and store it in variable
+		
+		if (i == 0){
+		printf( "%d ", number); // Print analog input i
+		}
+	}
+}
---
+++ Lab3/Gruppe44/Gruppe44/main.c
@@ -0,0 +1,50 @@
+/*
+ * Gruppe44.c
+ *
+ * Created: 02.09.2022 11:55:24
+ * Author : marbron
+ */ 
+
+#include <avr/io.h>
+#include "Communication.h"
+//#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define FOSC 1843200// Clock Speed
+#define BAUD 9600
+#define F_CPU 16000000
+#define MYUBRR FOSC/16/BAUD-1 // = 31
+
+#include "util/delay.h"
+
+
+
+
+int main(void)
+{
+	//FIX CODE SO THIS ONLY RUNS ONCE
+	USART_Init(31);
+	fdevopen(USART_Transmit, USART_Receive);
+	
+	adc_init();
+	
+	// USEFUL FUNCTIONS
+	//uint8_t adc_read(uint8_t channel);
+	//adc_calibrate();
+	//pos_t pos_read(void);
+	
+	// PINx & 0x01
+	
+	MCUCR |= (1 << SRE ); // enable XMEM
+	SFIOR |= (1 << XMM0 ); // mask unused bits
+	
+	MCUCR = 0x80; // Initialize SRAM
+	SFIOR = 0x20; 
+    while (1) 
+    {
+		
+		adc_read(); 
+		
+    }
+}
