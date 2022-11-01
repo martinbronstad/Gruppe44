57263479416bc632a012c79bd88896b217e12eb6
markuslangklopp <97596800+markuslangklopp@users.noreply.github.com>
2022-11-01 17:07:21 +0100

    Add files via upload

---
+++ Lab6/Node1/Gruppe44/Gruppe44/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC.c
+
+CAN.c
+
+Communication.c
+
+main.c
+
+OLED.c
+
+SPI.c
+
+SRAM.c
+
+XMEM.c
+
