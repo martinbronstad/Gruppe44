bcbd2cbe7f023a0d9372b3d75bca8302a460a079
markuslangklopp <97596800+markuslangklopp@users.noreply.github.com>
2022-11-11 15:23:57 +0100

    Lab7 Node 1 finished

---
+++ Lab7/Node1/Gruppe44/Gruppe44/Debug/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC.c
+
+CAN.c
+
+Communication.c
+
+Game.c
+
+Interruptcontroller.c
+
+main.c
+
+Menu.c
+
+OLED.c
+
+SPI.c
+
+SRAM.c
+
+XMEM.c
+
