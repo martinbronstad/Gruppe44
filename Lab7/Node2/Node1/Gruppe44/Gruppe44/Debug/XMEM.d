d9c0ecd23a9a207fc508837d5351f1b14dc8cb03
markuslangklopp <97596800+markuslangklopp@users.noreply.github.com>
2022-11-11 15:22:42 +0100

    Delete Lab7/Node2 directory

--- Lab7/Node2/Node1/Gruppe44/Gruppe44/Debug/XMEM.d
+++
@@ -1,7 +0,0 @@
-XMEM.d XMEM.o: .././XMEM.c \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
