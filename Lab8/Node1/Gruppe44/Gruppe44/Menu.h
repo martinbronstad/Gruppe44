3dba28b3c66e3d040985cb69f9d6558310ff0703
markuslangklopp <97596800+markuslangklopp@users.noreply.github.com>
2022-11-14 13:05:52 +0100

    Lab 8

---
+++ Lab8/Node1/Gruppe44/Gruppe44/Menu.h
@@ -0,0 +1,59 @@
+/*
+ * Menu.h
+ *
+ * Created: 07.11.2022 13:15:17
+ *  Author: marbron
+ */ 
+
+
+#ifndef MENU_H_
+#define MENU_H_
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include "util/delay.h"
+#include <math.h>
+
+
+struct Menu_Contents{
+	uint8_t menu_index;
+	char linedata[8][15];
+} Menu_contents;
+
+
+volatile bool neutral_flag;
+volatile bool main_menu_flag;
+volatile bool game_modes_flag;
+volatile bool difficulty_menu_flag;
+volatile bool game_over_flag;
+volatile bool play_again_flag;
+
+
+void flag_init();
+
+void Menu_init();
+
+void Main_menu();
+
+void Game_Modes_Menu();
+
+void Difficulty_Menu();
+
+void Game_Over_Menu();
+
+void Menu_print_arrow();
+
+void Menu_no_arrow();
+
+void Menu_arrow_refresh();
+
+void Arrow_refresh();
+
+void Arrow_refresh_updated();
+
+void Menu_print();
+
+void Change_page();
+
+
+#endif /* MENU_H_ */
